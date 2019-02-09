30babdb9add54f12336ae7baa74bdce9083ea5d6
Nathan Westfall <nathan@fistbumpstudios.com>
2019-02-09 12:53:45 -0500

    added more projects (c#)

---
+++ c#/console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var msg = "Hello World!";
+            Console.WriteLine(msg);
+        }
+    }
+}
